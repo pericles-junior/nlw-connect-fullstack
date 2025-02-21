c3de2f67c96c3e91c1782c322c2ae4cddbadee97
Péricles Jr <periclesmjr@hotmail.com>
2025-02-21 17:07:28 -0300

    iniciando o repositório

---
+++ app.js
@@ -0,0 +1,17 @@
+// comentários, variáveis, tipos de dados e funções
+// const - constante = não vai mudar
+// let - variável = pode mudar
+// let corBola
+// corBola = "azul"
+
+const corBola = "azul" + "escuro" //azulescuro
+const idade = 10 + 2 // 12
+
+// arrow function
+const soma = (a, b) => {
+    return a + b
+}
+
+const resultado = soma("alo", 20)
+
+alert (resultado)
