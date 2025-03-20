d69c8b800df170a08cfbc6b3bb1ee0590498ddb4
Péricles Jr <periclesmjr@hotmail.com>
2025-03-20 12:11:36 -0300

    armazenando novos usuários e adicionando controle de fuxo (if/else)

--- app.js
+++ app.js
@@ -1,20 +1,99 @@
 const app = document.getElementById('app')
 
+// array, vetor, lista
+const users = [
+    {
+        email: 'teste@teste.com',
+        phone: '999999999',
+        ref: 100,
+        refBy: null
+    },
+    {
+        email: 'teste2@teste.com',
+        phone: '999999999',
+        ref: 200,
+        refBy: 100
+    },
+    {
+        email: 'teste3@teste.com',
+        phone: '999999999',
+        ref: 300,
+        refBy: 100
+    },
+    {
+        email: 'teste4@teste.com',
+        phone: '999999999',
+        ref: 400,
+        refBy: 200
+]
+
+const getUser = (userData) => {
+    return users.find((user) => {
+        return user.email == userData.email
+    })
+}
+
+const getTotalSubscribers = (userData) => {
+    const subs = users.filter((user) => {
+        return user.refBy == userData.ref
+    })
+    return subs.length
+}
+const showInvite = (userData) => {
+    app.innerHTML = `
+    <input type="text" id="link" value="https://evento.com?ref=${userData.ref}" disabled>
+    
+    <div id="stats">
+        <h4>
+            ${getTotalSubscribers(userData)}
+        </h4>
+        <p>
+            Inscrições feitas
+        </p>
+    </div>
+    `
+}
+
+const saveUser = (userData) => {
+    const newUser = {
+        ...userData,
+        ref: Math.round(Math.random() * 4000),
+        refBy: 100
+    }
+
+    users.push(newUser)
+    return newUser
+}
+
 const formAction = () => {
     const form = document.getElementById('form')
     form.onsubmit = (event) => {
-        event.preventDefault
+        event.preventDefault()
+        const formData = new FormData(form)
+        const userData = {
+            email: formData.get('email'),
+            phone: formData.get('phone')
+        }
+
+        const user = getUser(userData)
+        if (user) {
+            showInvite(user)
+        } else {
+            const newUser = saveUser(userData)
+            showInvite(newUser)
+        }
     }
 }
 const startApp = () => {
     const content = `
     <form id="form">
-        <input type="email" name="email" placeholder="Email">
-        <input type="text" name="telefone" placeholder="Telefone">
+        <input type="email" name="email" placeholder="E-mail">
+        <input type="text" name="phone" placeholder="Telefone">
         <button>Confirmar</button>
     </form>`
 
     app.innerHTML = content
+    formAction()
 }
 
 startApp()
